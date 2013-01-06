f7e224a020ec71e937f86ef2f9aedba026915b74
shmp2005 <shmp2005@163.com>
2013-01-06 15:50:50 +0800

    fix bugs

---
+++ lib/fund/book.rb
@@ -0,0 +1,11 @@
+class Fund::Book
+  def publish
+    puts "hi, i am a book"
+  end
+
+  class << self
+    def read
+      puts "i want to read you"
+    end
+  end
+end
--- lib/yeetou.rb
+++ lib/yeetou.rb
@@ -3,11 +3,8 @@ require "yeetou/cat"
 require "yeetou/dog"
 require "yeetou/person"
 require "yeetou/yt/order"
+require "fund/book"
 
 module Yeetou
   # Your code goes here...
-end
-
-module Yt
-
 end
