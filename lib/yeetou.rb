3e4fff220b9f7032740f049e20fa799b2226e443
shmp2005 <shmp2005@163.com>
2013-01-06 15:26:04 +0800

    refactor it

--- lib/yeetou.rb
+++ lib/yeetou.rb
@@ -2,7 +2,7 @@ require "yeetou/version"
 require "yeetou/cat"
 require "yeetou/dog"
 require "yeetou/person"
-require "yt/order"
+require "yeetou/yt/order"
 
 module Yeetou
   # Your code goes here...
