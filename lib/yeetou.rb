9fd1f6115b43fc4be7777c986b9b447b4fc4bb6c
shmp2005 <shmp2005@163.com>
2013-01-06 14:16:39 +0800

    yt order

--- lib/yeetou.rb
+++ lib/yeetou.rb
@@ -2,6 +2,7 @@ require "yeetou/version"
 require "yeetou/cat"
 require "yeetou/dog"
 require "yeetou/person"
+require "yt/order"
 
 module Yeetou
   # Your code goes here...
---
+++ lib/yt/order.rb
@@ -0,0 +1,11 @@
+class Yt::Order
+  def to_s
+    puts "hi, i am an Order"
+  end
+
+  class << self
+    def pay
+      puts "i want to pay you"
+    end
+  end
+end
