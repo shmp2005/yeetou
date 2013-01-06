e2060fc366e889c7c6bc968adcd364360c89846b
shmp2005 <shmp2005@163.com>
2013-01-06 15:39:02 +0800

    fix bugs

--- lib/yeetou/yt/order.rb
+++ lib/yeetou/yt/order.rb
@@ -1,11 +1,15 @@
-class Yeetou::Yt::Order
-  def to_s
-    puts "hi, i am an Order"
-  end
+module Yeetou
+   module Yt
+      class Order
+	  def to_s
+	    puts "hi, i am an Order"
+	  end
 
-  class << self
-    def pay
-      puts "i want to pay you"
-    end
-  end
+	  class << self
+	    def pay
+	      puts "i want to pay you"
+	    end
+	  end
+      end
+  end	
 end
